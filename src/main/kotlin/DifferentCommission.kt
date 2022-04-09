2d57c173f3b7d99143bdab871d3aace567149192
NikolayN2147 <emercom2147@yandex.ru>
2022-04-10 00:05:13 +0300

    add comissionPrint

--- src/main/kotlin/DifferentCommission.kt
+++ src/main/kotlin/DifferentCommission.kt
@@ -1,7 +1,4 @@
-
-
-
-const val money: Int = 100
+const val money: Int = 1000100
 
 const val VK_PAY = "VkPay"
 const val MASTERCARD = "Mastercard"
@@ -11,20 +8,33 @@ const val MIR = "Mir"
 
 
 fun main() {
-commissionCalculation(money,VISA)
+    commissionCalculation(money, MIR)
 }
 
-fun commissionCalculation(money:Int,card:String){
-    val comissionMasterMaestro = money * 0.006 + 20
-    val comissionVisaMir = money * 0.0075
-    when(card){
-        VK_PAY-> println("Комиссия за перевод $money руб: 0 рублей 00 коп.")
-        MASTERCARD,MAESTRO -> println("Комиссия за перевод $money руб: "
-                + comissionMasterMaestro.toInt() + " руб " + ((comissionMasterMaestro * 100) % 100).toInt() + " коп" )
-        VISA,MIR -> if(comissionVisaMir < 35)
-            println("Комиссия за перевод $money руб: 35 рублей 00 коп.")
-        else
-            println("Комиссия за перевод $money руб: " + comissionVisaMir.toInt()
-        + " руб " + ((comissionVisaMir * 100) % 100).toInt() + " коп." )
+fun commissionCalculation(money: Int, card: String) {
+    val comissionMasterMaestro = 0.006
+    val comissionVisaMir = 0.0075
+    val comissionVkPay = 0.0
+    var comission: Double
+    when (card) {
+        VK_PAY -> {
+            comission = comissionVkPay * money
+            comissionPrint(comission)
+        }
+        MASTERCARD, MAESTRO -> {
+            comission = comissionMasterMaestro * money + 20
+            comissionPrint(comission)
+        }
+        VISA, MIR -> {
+            comission = comissionVisaMir * money
+            if (comission < 35) {
+                comission = 35.0
+                comissionPrint(comission)
+            } else comissionPrint(comission)
+        }
     }
+}
+
+fun comissionPrint(comission: Double) {
+    println("Комиссия за перевод : " + comission.toInt() + " руб " + ((comission * 100) % 100).toInt() + " коп. ")
 }
--- src/test/kotlin/DifferentCommissionKtTest.kt
+++ src/test/kotlin/DifferentCommissionKtTest.kt
@@ -6,5 +6,13 @@ class DifferentCommissionKtTest {
 
     @Test
     fun commissionCalculation() {
+        val rub = 100
+        val card = VISA
+
+        val result = commissionCalculation(
+            money, card
+        )
+
+        assertEquals(90,result)
     }
 }
