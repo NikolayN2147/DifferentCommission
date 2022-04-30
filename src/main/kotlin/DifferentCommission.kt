41b9effb24cf54acbe55e232a910d49e5ed5de9d
NikolayN2147 <100046842+NikolayN2147@users.noreply.github.com>
2022-04-30 21:36:13 +0300

    rework

--- src/main/kotlin/DifferentCommission.kt
+++ src/main/kotlin/DifferentCommission.kt
@@ -1,40 +1,34 @@
-
-const val money: Int = 10000
-
-var comission: Double = 0.0
-
-const val VK_PAY = 0.0
-const val MASTERCARD_MAESTRO = 0.006
-const val VISA_MIR = 0.0075
+const val money: Int = 100_000
+const val VK_PAY = "VK_PAY"
+const val MASTERCARD = "MASTERCARD"
+const val MAESTRO = "MAESTRO"
+const val VISA = "VISA"
+const val MIR = "MIR"
 
 fun main() {
-    commissionCalculation(money, VISA_MIR)
+   val result = commissionCalculation(VISA,0,0)
+    comissionPrint(result)
 }
 
-fun commissionCalculation(money: Int, card: Double) {
-
+fun commissionCalculation(card: String, limitOfDay: Int = 0, limitOfMonth: Int = 0):Double {
+    var result = 0.0
     when (card) {
         VK_PAY -> {
-            comission = VK_PAY * money
-            comissionPrint(comission)
+            if (limitOfDay > 15_000 || limitOfMonth > 40_000) result = 0.0
         }
-        MASTERCARD_MAESTRO -> {
-            comission = MASTERCARD_MAESTRO * money + 20
-            comissionPrint(comission)
+        MASTERCARD, MAESTRO -> {
+            result = if (money < 75_000) 0.0 else 0.006 * money + 20
         }
-        VISA_MIR -> {
-            comission = VISA_MIR * money
-            if (comission < 35) {
-                comission = 35.0
-                comissionPrint(comission)
-            } else {
-                comission = VISA_MIR * money
-                comissionPrint(comission)
-            }
+        VISA, MIR -> {
+            result = if (0.0075 * money < 35) 35.0 else 0.0075 * money
+
         }
     }
+    return result
+
 }
 
+
 fun comissionPrint(comission: Double) {
-    println("Комиссия за перевод : " + comission.toInt() + " руб " + ((comission * 100) % 100).toInt() + " коп. ")
+   println("Комиссия за перевод : " + comission.toInt() + " руб " + ((comission * 100) % 100).toInt() + " коп. ")
 }
--- src/test/kotlin/DifferentCommissionKtTest.kt
+++ src/test/kotlin/DifferentCommissionKtTest.kt
@@ -3,44 +3,15 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class DifferentCommissionKtTest {
-
     @Test
-    fun commissionCalculation_VkPay() {
+    fun commissionCalculation() {
+        val card = "VISA"
+        val limitOfDay = 0
+        val limitOfMonth = 0
 
-        val comission = VK_PAY
-        val moneyVk = money
-
-        val result  = comission * moneyVk
-
-        assertEquals(0,result.toInt())
-
+        val result = commissionCalculation(card,limitOfDay,limitOfMonth)
+        val expected = 750
+        assertEquals(expected, result.toInt())
 
     }
-
-    @Test
-    fun commissionCalculation_Master_Maestro() {
-
-        val comissionVkpay = MASTERCARD_MAESTRO
-        val moneyMaster = money
-
-        val result  = comissionVkpay * moneyMaster
-
-        assertEquals(60,result.toInt())
-
-
-    }
-
-    @Test
-    fun commissionCalculation_Visa_Mir() {
-
-        val comissionVkpay = VISA_MIR
-        val moneyVisa = money
-
-        val result  = comissionVkpay * moneyVisa
-
-        assertEquals(75,result.toInt())
-
-
-    }
-
 }
